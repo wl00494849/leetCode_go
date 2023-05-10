d36a4783fe725e9038ec1d7f0a1fcf1debf61437
zhung1oz7 <55905379+wl00494849@users.noreply.github.com>
2023-05-10 18:19:47 +0800

    spiral-matrixII

--- spiral-matrixII/spiral-matrixII.go
+++ spiral-matrixII/spiral-matrixII.go
@@ -1,7 +1,40 @@
 package spiralmatrixii
 
 func generateMatrix(n int) [][]int {
-	res := [][]int{}
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, n)
+	}
+
+	var startRow int
+	var startCol int
+	endCol := n - 1
+	endRow := n - 1
+	total := n * n
+	count := 1
+
+	for count <= total {
+		for i := startCol; i <= endCol && count <= total; i++ {
+			res[startRow][i] = count
+			count++
+		}
+		startRow++
+		for i := startRow; i <= endRow && count <= total; i++ {
+			res[i][endCol] = count
+			count++
+		}
+		endCol--
+		for i := endCol; i >= startCol && count <= total; i-- {
+			res[endRow][i] = count
+			count++
+		}
+		endRow--
+		for i := endRow; i >= startRow && count <= total; i-- {
+			res[i][startCol] = count
+			count++
+		}
+		startCol++
+	}
 
 	return res
 }
