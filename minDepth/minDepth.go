ed4cd67fa2e57eb30f53ab31d57599fc1579f9c5
zhung1oz7 <55905379+wl00494849@users.noreply.github.com>
2023-07-11 13:11:09 +0800

    minDepth

---
+++ minDepth/minDepth.go
@@ -0,0 +1,33 @@
+package mindepth
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+	if root.Left != nil && root.Right != nil {
+		return 1 + min(left, right)
+	}
+	return 1 + max(left, right)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
